c670d5eb0d53984447d86ea6725faae70a9f3e1e
NiclasCramer <niclascramer@gmail.com>
2022-10-24 19:11:55 +0200

    delete Archive

--- Archiv/test.py
+++
@@ -1,6 +0,0 @@
-import os
-#open -a "Google Chrome" http://127.0.0.1:8082/ && python manage.py runserver
-a=os.system('cmd /k "start  http://127.0.0.1:8082/"')
-b=os.system('cmd /k "python multi_app.py"')
-print(b)
-print(a)
